d26e12140a53bb7418c36837e0978be44a28b035
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-01-12 03:35:43 +0100

    Fix indentation

--- src/basic_models/sequence_generation.py
+++ src/basic_models/sequence_generation.py
@@ -64,18 +64,18 @@ class BasicSequenceGenerator(Module):
         # labels.shape = (B, L)
         return batch_output, y
 
-  def predict_with_model(self, X: SimpleDataset) -> list[ndarray]:
-      self.eval()
-      dataloader = FieldBatchDataloader(X, device=self.device, batch_size=32)
-      answer: list[ndarray] = [None] * len(X)
-      for batch in tqdm(dataloader):
-          indexes = batch["indexes"]
-          with torch.no_grad():
-              batch_answer = self(batch['phon'], None, True)
-          labels = batch_answer["labels"].cpu().numpy()
-          # probs = batch_answer.cpu().numpy()
-          # labels = probs.argmax(axis=-1)
-          for index, curr_labels in zip(indexes, labels, strict=True):
-              result = np.take(X.vocabs["morphon"].symbols_, curr_labels)
-              answer[index] = result
-      return answer
+    def predict_with_model(self, X: SimpleDataset) -> list[ndarray]:
+        self.eval()
+        dataloader = FieldBatchDataloader(X, device=self.device, batch_size=32)
+        answer: list[ndarray] = [None] * len(X)
+        for batch in tqdm(dataloader):
+            indexes = batch["indexes"]
+            with torch.no_grad():
+                batch_answer = self(batch['phon'], None, True)
+            labels = batch_answer["labels"].cpu().numpy()
+            # probs = batch_answer.cpu().numpy()
+            # labels = probs.argmax(axis=-1)
+            for index, curr_labels in zip(indexes, labels, strict=True):
+                result = np.take(X.vocabs["morphon"].symbols_, curr_labels)
+                answer[index] = result
+        return answer
