c557041e37f176c3a7c16164fc376a1b22023c03
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-02-01 17:42:06 +0100

    Add functions for simple evaluation with multiple possible correct segmentations

---
+++ src/evaluation.py
@@ -0,0 +1,25 @@
+from typing import TypedDict
+from models.morphon.transducer.basic import BasicMorphonologicalTransducer
+
+class Word(TypedDict):
+  phon: str
+  morphon: list[str]
+
+def read_data_all_corr(file: str) -> list[Word]:
+    return [{'phon': token,
+             'morphon': segmentations.split(', ')}
+            for token, segmentations
+             in map(lambda line: line.split('\t'), read_list(f'{LANG}.word.{file}.tsv'))]
+
+def evaluate(segmentations: list[str], words: list[Word]):
+  correct = 0
+  for (segmentation, word) in zip(segmentations, words, strict=True):
+    if segmentation in word['morphon']:
+      correct += 1
+  accuracy = 100 * correct / len(words)
+  print('Accuracy: {0:.2f}'.format(accuracy))
+
+def test(model: BasicMorphonologicalTransducer, words: list[Word]):
+  input = [word['phon'] for word in words]
+  segmentations = model.apply(input)
+  evaluate(segmentations, words)
