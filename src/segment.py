e15acf42c5fd797f97d4d9845201b2aa98e59adb
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-02-18 22:46:18 +0100

    Take a list of words as input

--- src/segment.py
+++ src/segment.py
@@ -7,7 +7,7 @@ from utils.dataloader import DEVICE
 import torch
 parser = argparse.ArgumentParser(
   prog='segment.py',
-  description='Segment a word in the specified language with the specified model'
+  description='Segment words in the specified language with the specified model'
 )
 parser.add_argument('language', choices=listdir('models'),
                     help='the three-letter code of the language')
@@ -15,8 +15,8 @@ parser.add_argument('model_type', choices=['tagger', 'transducer'],
                     help='the general type of the model to use')
 parser.add_argument('model_subtype', choices=['CNN', 'LSTM', 'RCNN', 'RCNN-skip-conn'],
                     help='the subtype of the model to use')
-parser.add_argument('word',
-                    help='the word to segment')
+parser.add_argument('words', nargs='*',
+                    help='the words to segment (multiple values allowed)')
 args = parser.parse_args()
 model_dir = path.join('models', args.language, args.model_type, args.model_subtype)
 vocab_dir = path.join(model_dir, 'Vocabularies')
@@ -28,6 +28,6 @@ model = make_model(args.model_type, args.model_subtype, vocabs, DEVICE)
 checkpoint_dir = path.join(model_dir, 'Checkpoints', '0')
 checkpoint_file = path.join(checkpoint_dir, f'checkpoint_best_{args.model_subtype}.pt')
 model.load_state_dict(torch.load(checkpoint_file, map_location=DEVICE))
-result = model.apply([args.word])
+result = model.apply(args.words)
 for segmentation in result:
     print(segmentation)
