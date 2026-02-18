6f5b7e0741905c79c0b6013f554f29d179a5892f
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-02-18 23:23:53 +0100

    Add a script for applying the models to the data in the canonical-segmentation repository

---
+++ predict_canonical-segmentation.sh
@@ -0,0 +1,16 @@
+function predict_for_language() {
+  code="$1"
+  language="$2"
+  infile="canonical-segmentation/$language/test0"
+  for model in $(ls "models/$code"); do
+    for subtype in $(ls "models/$code/$model"); do
+      outdir="predictions/$code/$model/$subtype"
+      mkdir -p "$outdir"
+      outfile="$outdir/predictions.txt"
+      cut -f 1 "$infile" | env/bin/python src/segment.py "$code" "$model" "$subtype" > "$outfile"
+    done
+  done
+}
+predict_for_language eng english
+predict_for_language deu german
+predict_for_language ind indonesian
