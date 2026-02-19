a52161feff83f8a6e8db3acc02cf558b533c2aa9
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-02-19 20:20:03 +0100

    Compute accuracies for the English, German and Indonesian models

---
+++ src/compute_accuracy.py
@@ -0,0 +1,54 @@
+import os
+from os import path
+from library.read import read_list
+import pandas as pd
+from sklearn.metrics import accuracy_score
+import json
+
+LANGUAGE_CODES = ['deu', 'eng', 'ind']
+
+CODE_TO_LANGUAGE = {
+  'deu': 'german',
+  'eng': 'english',
+  'ind': 'indonesian'
+}
+
+DATASET_PATH = 'canonical-segmentation'
+CORR_FILE = 'test0'
+PRED_DIR = 'predictions'
+PRED_FILE = 'predictions.txt'
+OUTDIR = 'metrics'
+OUTFILE = 'Accuracy.json'
+
+accuracies = dict[str, dict[str, float]]()
+
+for code in LANGUAGE_CODES:
+  accuracies[code] = dict[str, float]()
+  language_dir = path.join(PRED_DIR, code)
+  language = CODE_TO_LANGUAGE[code]
+  corr_segm_dir = path.join(DATASET_PATH, language)
+  corr_file = path.join(corr_segm_dir, CORR_FILE)
+  daraFrame = pd.read_csv(corr_file, sep='\t', names=['orth', 'morphon', 'segm'])
+  y_true = daraFrame['segm'].to_list()
+  for model_type in os.listdir(language_dir):
+    model_type_dir = path.join(language_dir, model_type)
+    for model_subtype in os.listdir(model_type_dir):
+      model_identifier = '{0}_{1}'.format(model_type, model_subtype)
+      model_dir = path.join(model_type_dir, model_subtype)
+      pred_file = path.join(model_dir, PRED_FILE)
+      predictions = read_list(pred_file)
+      match model_type:
+        case 'tagger':
+          postprocess = lambda segm: segm.replace('@', ' ')
+        case 'transducer':
+          postprocess = lambda segm: segm.replace('-', ' ')
+        case _:
+          raise ValueError('Unssuuported model type: {0}.'.format(model_type))
+      y_pred = list(map(postprocess, predictions))
+      accuracy: float = accuracy_score(y_true, y_pred)
+      accuracies[code][model_identifier] = round(100 * accuracy, 2)
+
+os.makedirs(OUTDIR, exist_ok=True)
+outfile = path.join(OUTDIR, OUTFILE)
+with open(outfile, 'w', encoding='utf-8') as fout:
+  json.dump(accuracies, fout, ensure_ascii=False, indent='\t')
--- src/library/__init__.py
+++ src/library/__init__.py
@@ -4,9 +4,9 @@ extensions = ['.txt', '.xml', '.lex', '.csv', '.lfg']
 
 default_dir = None
 
-def get_file_name(file_name: str, use_default_dir = False) -> str:
+def get_file_name(file_name: str, use_default_dir = False, add_extension: bool = False) -> str:
     if use_default_dir and default_dir is not None and not file_name.startswith('/'):
         file_name = path.join(default_dir, file_name)
-    if '.' not in file_name:
+    if add_extension and '.' not in file_name:
         file_name += '.txt'
     return file_name
