31fd6c64d2582729c73525ff468b4dba716adf17
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-03-02 16:10:21 +0100

    Use awk instead of tr for lowercasing in order to be able to lowercase letters with Umlaut

--- extract_words_with_prefixes.sh
+++ extract_words_with_prefixes.sh
@@ -1,3 +1,3 @@
 lang="$1"
 prefixes="$2"
-for prefix in $prefixes; do cut -f 1 "unimorph/$lang/$lang" | tr '[:upper:]' '[:lower:]' | grep "^$prefix" | sort | uniq > "examples/$lang/$prefix.txt"; done
+for prefix in $prefixes; do cut -f 1 "unimorph/$lang/$lang" | awk '{print tolower($0)}' | grep "^$prefix" | sort | uniq > "examples/$lang/$prefix.txt"; done
