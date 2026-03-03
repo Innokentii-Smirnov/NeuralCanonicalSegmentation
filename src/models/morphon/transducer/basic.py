9c294654c43e536b7d574e1d379c1e0c00c12407
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-03-03 09:50:03 +0100

    Move the predict function from basic sequence generator to basic morphonological transducer, do not inherit in the latter from the former

--- src/models/morphon/transducer/basic.py
+++ src/models/morphon/transducer/basic.py
@@ -1,15 +1,38 @@
 import torch
-from basic_models.sequence_generation import BasicSequenceGenerator
+import torch.nn as nn
+import numpy as np
+from numpy import ndarray
+from tqdm.auto import tqdm
 from utils.vocabulary import Vocabulary
 from utils.dataset import SimpleDataset
+from utils.dataloader import FieldBatchDataloader
 from stringutils import string_to_list
 
-class BasicMorphonologicalTransducer(BasicSequenceGenerator):
+class BasicMorphonologicalTransducer(nn.Module):
 
     def __init__(self, vocabularies: dict[str, Vocabulary], device: torch.device):
-        super(BasicMorphonologicalTransducer, self).__init__(device)
+        super(BasicMorphonologicalTransducer, self).__init__()
+        self.device = device
+        if self.device is not None:
+            self.to(self.device)
         self.vocabularies = vocabularies
         self.combine_diacritics = self.vocabularies['phon'].contains_string_with_combined_diacritic()
+
+    def predict(self, X: SimpleDataset) -> list[ndarray]:
+        self.eval()
+        dataloader = FieldBatchDataloader(X, device=self.device, batch_size=32)
+        answer: list[ndarray] = [None] * len(X)
+        for batch in tqdm(dataloader):
+            indexes = batch["indexes"]
+            with torch.no_grad():
+                batch_answer = self(batch['phon'], None, True)
+            labels = batch_answer["labels"].cpu().numpy()
+            # probs = batch_answer.cpu().numpy()
+            # labels = probs.argmax(axis=-1)
+            for index, curr_labels in zip(indexes, labels, strict=True):
+                result = np.take(X.vocabs["morphon"].symbols_, curr_labels)
+                answer[index] = result
+        return answer
 
     def apply_to(self, words: list[str]) -> list[str]:
         data = [{'phon': string_to_list(word, self.combine_diacritics)} for word in words]
