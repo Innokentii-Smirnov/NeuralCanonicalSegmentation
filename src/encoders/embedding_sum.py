1a535cd897908af6c03943c139778e916361929b
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-04-03 01:22:37 +0200

    Add nn.Module constructor call, correct the dimension over which to sum

--- src/encoders/embedding_sum.py
+++ src/encoders/embedding_sum.py
@@ -5,9 +5,10 @@ import torch.nn as nn
 class EmbeddingSumEncoder(nn.Module):
 
   def __init__(self, vocabulary_size: int, embedding_dim: int):
+    super(EmbeddingSumEncoder, self).__init__()
     self.embedding = nn.Embedding(vocabulary_size, embedding_dim)
 
   def forward(self, elements: Tensor) -> Tensor:
     embeddings = self.embedding(elements)
-    embedding_sum = torch.sum(embeddings, dim=-1)
+    embedding_sum = torch.sum(embeddings, dim=-2)
     return embedding_sum
