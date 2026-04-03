99ab6cfc36187a9e73ae5acbc8719b33c9d8da1a
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-04-03 17:51:51 +0200

    Add scripts for aligning the Hurrian data (with character-pair-specific replacement costs)

---
+++ align_Hurrian_data.sh
@@ -0,0 +1,2 @@
+rm -r aligned_data/Levenshtein/Hurrian
+dotnet run --project LevenshteinAlignment Hurrian aligned_data/Levenshtein/Hurrian alignment_costs/xhu.txt
---
+++ align_Hurrian_data_and_print_alignments.sh
@@ -0,0 +1,1 @@
+clear && ./align_Hurrian_data.sh && ./print_Hurrian_alignments.sh
