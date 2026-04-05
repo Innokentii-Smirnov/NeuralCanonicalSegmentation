e8422ab3bb40f1634fd6054b78f0da6eb466e693
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-04-05 12:42:35 +0200

    Print out aligned development data

--- print_Hurrian_alignments.sh
+++ print_Hurrian_alignments.sh
@@ -1,1 +1,2 @@
 env/bin/python src/print_alignments.py aligned_data/Levenshtein/Hurrian/xhu.word.train.tsv formatted_alignments/Levenshtein/xhu
+env/bin/python src/print_alignments.py aligned_data/Levenshtein/Hurrian/xhu.word.dev.tsv formatted_alignments/Levenshtein/xhu-dev
