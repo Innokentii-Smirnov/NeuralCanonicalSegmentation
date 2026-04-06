5a410b4745a765b4c646b9ac7b2d14e5cc423395
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-04-06 17:16:39 +0200

    Compute morpheme F1-measure as an additional metric

---
+++ src/morpheme_f1.py
@@ -0,0 +1,24 @@
+import re
+import sys
+sys.path.insert(1, '2022SegmentationST/evaluation')
+from evaluate import n_correct
+morpheme_boundary = re.compile(r'(?<=[.+-=@])')
+
+def compute_f1(corr_segmentations: list[str], pred_segmentations: list[str]) -> float:
+  total_corr = 0
+  total_pred = 0
+  total_overlaps = 0
+  for pred_segmentation, corr_segmentation in zip(pred_segmentations, corr_segmentations, strict=True):
+    corr_segments = morpheme_boundary.split(corr_segmentation)
+    pred_segments = morpheme_boundary.split(pred_segmentation)
+    total_corr += len(corr_segments)
+    total_pred += len(pred_segments)
+    n_overlaps = n_correct('|'.join(corr_segments), '|'.join(pred_segments))
+    total_overlaps += n_overlaps
+  precision = 100 * total_overlaps / total_pred
+  recall = 100 * total_overlaps / total_corr
+  if precision+recall == 0:
+      f_measure = .0
+  else:
+      f_measure = 2 * precision * recall / (precision + recall)
+  return f_measure
