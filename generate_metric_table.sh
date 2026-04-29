d3ce52925730f04b67eee655773012c63fcc3eb9
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-04-29 15:56:48 +0200

    Includecharacter transformers trained with batch size 128 into the metric table

--- generate_metric_table.sh
+++ generate_metric_table.sh
@@ -3,6 +3,6 @@ env/bin/python src/generate_metric_table.py metrics/Accuracy.json \
   --models tagger_{CNN,LSTM,RCNN,RCNN-skip-conn} \
            hard-attention_CLUZH \
            transducer_LSTM \
-           transformer_char \
+           transformer_{char,char_bs128} \
   | xclip -selection clipboard
 xclip -selection clipboard -o
