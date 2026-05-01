dd1150da56064537230855a4747470d88cf8d4dd
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-05-01 16:28:28 +0200

    Include Hurrian in the metric table

--- generate_metric_table.sh
+++ generate_metric_table.sh
@@ -1,5 +1,5 @@
 env/bin/python src/generate_metric_table.py metrics/Accuracy.json \
-  --languages chu eng deu ind \
+  --languages chu eng deu ind xhu \
   --models tagger_{CNN,LSTM,RCNN,RCNN-skip-conn} \
            hard-attention_CLUZH \
            transducer_LSTM \
