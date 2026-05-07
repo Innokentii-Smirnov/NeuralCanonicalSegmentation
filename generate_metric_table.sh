09fa56993239bd0ae3f334ee6fb1eabb4dfe38c7
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-05-07 16:26:47 +0200

    Make the language codes a command-line argument of the metric table generation script

--- generate_metric_table.sh
+++ generate_metric_table.sh
@@ -1,6 +1,10 @@
 metric="$1"
+language_codes="$2"
+if [ -z "$language_codes" ]; then
+  language_codes="chu eng deu ind xhu"
+fi
 env/bin/python src/generate_metric_table.py "metrics/$metric.json" \
-  --languages chu eng deu ind xhu \
+  --languages $language_codes \
   --models tagger_{CNN,LSTM,RCNN,RCNN-skip-conn} \
            hard-attention_CLUZH \
            transducer_LSTM \
