817b9d7f6ba94467dab343ea578f2c2eaba1694d
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-05-07 14:38:02 +0200

    Make the language codes a command-line argument

--- src/compute_f1.py
+++ src/compute_f1.py
@@ -1,8 +1,16 @@
+from argparse import ArgumentParser
 from metric_computation import compute_metrics, MetricFunction
 from morpheme_f1 import compute_f1
 
 LANGUAGE_CODES = ['chu', 'eng', 'deu', 'ind', 'xhu', 'ces']
 
+parser = ArgumentParser(
+  prog='compute_f1.py',
+  description='Compute F1-measure for canonical morpheme segmentation'
+)
+parser.add_argument('--language-codes', nargs='*', default=LANGUAGE_CODES)
+args = parser.parse_args()
+
 METRIC_FUNCTION: MetricFunction = compute_f1
 
-compute_metrics(METRIC_FUNCTION, LANGUAGE_CODES)
+compute_metrics(METRIC_FUNCTION, args.language_codes)
