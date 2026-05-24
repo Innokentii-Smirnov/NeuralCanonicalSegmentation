b30b0cc8621c38a2a3ad9c421a89b3a087b6817d
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-05-24 09:35:37 +0200

    Add the extension .txt when a file without extension does not exist

--- src/library/__init__.py
+++ src/library/__init__.py
@@ -7,6 +7,7 @@ default_dir = None
 def get_file_name(file_name: str, use_default_dir = False, add_extension: bool = False) -> str:
     if use_default_dir and default_dir is not None and not file_name.startswith('/'):
         file_name = path.join(default_dir, file_name)
-    if add_extension and '.' not in file_name:
+    if (add_extension and '.' not in file_name or
+        not path.exists(file_name) and path.exists(file_name + '.txt')):
         file_name += '.txt'
     return file_name
